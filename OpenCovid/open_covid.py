0d308622cbea310885d4a1b2fc344113eaee22cc
hd9189 <hzfmd2006@gmail.com>
2022-05-08 19:23:08 -0400

    new commit

--- OpenCovid/open_covid.py
+++ OpenCovid/open_covid.py
@@ -1,3 +1,4 @@
+from matplotlib.legend import Legend
 import requests, json
 import pandas as pd
 #pylot is a function within matplotlib, so we import that specifically
@@ -26,6 +27,7 @@ plt.show()
 #         #prints no decimal points, and with thousand seperator
 #         print(f'{province["province_full"]}: {province["pop"]:,.0f}\n')
 
+
 def population():
     url = 'https://api.opencovid.ca/other?stat=prov'
     response = requests.get(url)
@@ -38,16 +40,58 @@ def population():
     #prints the data set in a more organized manner
     print(df)
     #creaters a plot and sets the x and y axis
-    df.plot.bar(
-        x='province',
-        y='pop'
+    ax = df.plot.bar(
+        x='Province',
+        y='Population',
+        rot=45,
+        legend=False
     )
+    ax.set_title()
     plt.show()
 
-# population()
+def millions(x, pos):
+    return f'{x/1000000:,.0f}' #display by million
+
 def help():
     df = pd.read_csv('Population.csv')
     print(df)
-    ax = df.plot.bar() #plots df csv
+    ax = df.plot.bar(
+        x='Province',
+        y='Population',
+        rot=45,
+        legend=False
+    ) #plots df csv
+    ax.set_title('Population by Province (in millions)', fontsize=16)
+    ax.set_xlabel('Province', fontsize=14)
+    ax.set_ylabel('Population', fontsize=14)
+    ax.yaxis.set_major_formatter(millions) #millions is self made function, formats the y axis
     plt.show()
-help()
+
+# population()
+# help()
+
+def thousands(x, pos):
+    return f'{x:,.0f}'
+
+def province(prov):
+    url = 'https://api.opencovid.ca/timeseries'
+    response = requests.get(url)
+    json_data = json.loads(response.text)
+    df = pd.read_json(json.dumps(json_data['data']['cases'])) #dumps converts json into string
+    ax = df.query(f'region == "{prov}"').plot( #creates a line graph
+        x = 'date',
+        y = 'value_daily',
+        rot = 45,
+        legend = False
+    )
+    ax.set_title(f'Daily Cases in {prov}')
+    ax.set_xlabel('Date', fontsize=14)
+    ax.set_ylabel('Number of People', fontsize=14)
+    ax.yaxis.set_major_formatter(thousands)
+    plt.show()
+
+provinces = ["AB", "BC", "ON", "MB","NB", "NL", "NS", "NT", "NU", "PE", "QC", "SK", "YT"]
+prov = input(f"Choose a province {provinces}: ")
+while prov not in provinces:
+    prov = input(f"Choose a province {provinces}: ")
+province(prov)
---
+++ test.py
@@ -0,0 +1,5 @@
+
+json_data = [1, 2]
+json_string = '[1, 2]'
+print(json_data)
+print(json_string)
