467da91d0f64494490c91d74a9d6f84a48d9d54b
hd9189 <hzfmd2006@gmail.com>
2022-05-15 18:34:47 -0400

    new commit

--- HydroBilling/hyrobilling.py
+++ HydroBilling/hyrobilling.py
@@ -1,4 +1,20 @@
+from matplotlib.legend import Legend
+import requests, json
 import pandas as pd
 from matplotlib import pyplot as plt
 plt.show()
 
+def billing():
+    df = pd.read_csv('Hydro_Billing.csv')
+    ax = df.plot.bar(
+        x='From (yyyy-mm-dd)',
+        y='Amount Due',
+        rot = 45,
+        legend=False
+    )
+    ax.set_title('Hydrobill', fontsize=16)
+    ax.set_xlabel('Date (yyyy-mm-dd)', fontsize=14)
+    ax.set_ylabel('Amount ($)', fontsize=14)
+    plt.show()
+
+billing()
--- test.py
+++
@@ -1,5 +0,0 @@
-
-json_data = [1, 2]
-json_string = '[1, 2]'
-print(json_data)
-print(json_string)
